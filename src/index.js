e0589a0ab60c65b4405711368b53192ada8b79ce
Parvin Musayev <115371722+ParvinMusayev@users.noreply.github.com>
2023-12-30 23:16:56 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom/client";
 import Header from "./Components/Layout/Header";
 import ContactIndex from "./Components/ContactPages/ContactIndex";
 
+// sadik turan
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<ContactIndex />);
